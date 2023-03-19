fd0da21963484f4cb9286f7b2669d08094695a9f
qwopqwop200 <64115820+qwopqwop200@users.noreply.github.com>
2023-03-20 02:06:17 +0900

    Update llama_inference.py

--- llama_inference.py
+++ llama_inference.py
@@ -70,6 +70,10 @@ if __name__ == '__main__':
         help='#bits to use for quantization; use 16 for evaluating base model.'
     )
     parser.add_argument(
+        '--groupsize', type=int, default=-1,
+        help='Groupsize to use for quantization; default uses full row.'
+    )
+    parser.add_argument(
         '--load', type=str, default='',
         help='Load quantized model.'
     )
