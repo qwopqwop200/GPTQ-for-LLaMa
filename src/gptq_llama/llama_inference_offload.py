8e702d172e976906cb3f74aaf22b70daf5353216
Star Dorminey <sterlind@microsoft.com>
2023-03-25 19:51:55 -0700

    Fix relative imports.

--- src/gptq_llama/llama_inference_offload.py
+++ src/gptq_llama/llama_inference_offload.py
@@ -3,9 +3,9 @@ import time
 import torch
 import torch.nn as nn
 
-from gptq import *
-from modelutils import *
-from quant import *
+from .gptq import *
+from .modelutils import *
+from .quant import *
 
 from transformers import AutoTokenizer
 
