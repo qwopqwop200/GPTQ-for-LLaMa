c57aaea9979faf098fd02958d93f96f4557b16ef
Antti Mäkelä <11278294+MasterTaffer@users.noreply.github.com>
2023-03-27 09:11:27 +0300

    default kernel_switch_threshold to 128

--- quant.py
+++ quant.py
@@ -136,7 +136,7 @@ except:
 
 # Assumes layer is perfectly divisible into 256 * 256 blocks
 class QuantLinear(nn.Module): 
-    def __init__(self, bits, groupsize, infeatures, outfeatures, faster=False, kernel_switch_threshold=None):
+    def __init__(self, bits, groupsize, infeatures, outfeatures, faster=False, kernel_switch_threshold=128):
         super().__init__()
         if bits not in [2,3,4,8]:
             raise NotImplementedError("Only 2,3,4,8 bits are supported.")
