4f04034bd8366ba8bcdc12e492f87f35166b901b
qwopqwop200 <qwopqwop200@gmail.com>
2023-03-28 20:08:47 +0900

    Update llama.py

--- llama.py
+++ llama.py
@@ -260,9 +260,9 @@ def load_quant(model, checkpoint, wbits, groupsize=-1):
     print('Loading model ...')
     if checkpoint.endswith('.safetensors'):
         from safetensors.torch import load_file as safe_load
-        model.load_state_dict(safe_load(checkpoint))
+        model.load_state_dict(safe_load(checkpoint), strict = False)
     else:
-        model.load_state_dict(torch.load(checkpoint))
+        model.load_state_dict(torch.load(checkpoint), strict = False)
     model.seqlen = 2048
     print('Done.')
 
