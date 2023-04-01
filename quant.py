bf7db011403ead37c10f52f2ce4b2a0fcdcc6135
qwopqwop200 <qwopqwop200@gmail.com>
2023-04-01 15:46:33 +0900

    Update quant.py

--- quant.py
+++ quant.py
@@ -127,14 +127,6 @@ class Quantizer(nn.Module):
 
     def ready(self):
         return torch.all(self.scale != 0)
-
-
-try:
-    import quant_cuda
-    is_cuda = True
-except:
-    print('CUDA extension not installed.')
-    is_cuda = False
     
 try:
     import triton
@@ -237,10 +229,8 @@ try:
         c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
         c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
         tl.store(c_ptrs, accumulator, mask=c_mask)
-    is_trioton = True
 except:
     print('trioton not installed.')
-    is_trioton = False
         
 def make_quant(module, names, bits, groupsize, name=''):
     if isinstance(module, QuantLinear):
@@ -255,10 +245,10 @@ def make_quant(module, names, bits, groupsize, name=''):
         make_quant(child, names, bits, groupsize, name + '.' + name1 if name != '' else name1)
 
 class QuantLinear(nn.Module): 
-    def __init__(self, bits, groupsize, infeatures, outfeatures, bias, kernel_switch_threshold=128, is_trioton=is_trioton, is_cuda=is_cuda):
+    def __init__(self, bits, groupsize, infeatures, outfeatures, bias):
         super().__init__()
-        if bits not in [2,3,4,8]:
-            raise NotImplementedError("Only 2,3,4,8 bits are supported.")
+        if bits not in [2,4,8]:
+            raise NotImplementedError("Only 2,4,8 bits are supported.")
         self.infeatures = infeatures
         self.outfeatures = outfeatures
         self.bits = bits
@@ -274,18 +264,6 @@ class QuantLinear(nn.Module):
         else:
             self.bias = None
         
-        # is performed by unpacking the weights and using torch.matmul
-        if self.bits in [2,4,8]: 
-            self.register_buffer('wf',torch.tensor(list(range(0,32,self.bits)), dtype=torch.int32).unsqueeze(0),persistent=False)
-        elif self.bits == 3:
-            self.register_buffer('wf', torch.tensor([[0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 0],
-                                                     [0, 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31],
-                                                     [0, 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 0],], dtype=torch.int32).reshape(1,3,12), persistent=False)
-            
-        self.kernel_switch_threshold = kernel_switch_threshold
-        self.is_trioton = is_trioton
-        self.is_cuda = is_cuda
-
     def pack(self, linear, scales, zeros, g_idx = None):
         self.g_idx = g_idx.clone() if g_idx is not None else self.g_idx
         
@@ -302,9 +280,7 @@ class QuantLinear(nn.Module):
         intweight = torch.cat(intweight,dim=1)
         intweight = intweight.t().contiguous()
         intweight = intweight.numpy().astype(np.uint32)
-        qweight = np.zeros(
-            (intweight.shape[0] // 32 * self.bits, intweight.shape[1]), dtype=np.uint32
-        )
+        qweight = np.zeros((intweight.shape[0] // 32 * self.bits, intweight.shape[1]), dtype=np.uint32)
         i = 0
         row = 0
         while row < qweight.shape[0]:
@@ -313,27 +289,8 @@ class QuantLinear(nn.Module):
                     qweight[row] |= intweight[j] << (self.bits * (j - i))
                 i += 32//self.bits
                 row += 1
-            elif self.bits == 3:
-                for j in range(i, i + 10):
-                    qweight[row] |= intweight[j] << (3 * (j - i))
-                i += 10
-                qweight[row] |= intweight[i] << 30
-                row += 1
-                qweight[row] |= (intweight[i] >> 2) & 1
-                i += 1
-                for j in range(i, i + 10):
-                    qweight[row] |= intweight[j] << (3 * (j - i) + 1)
-                i += 10
-                qweight[row] |= intweight[i] << 31
-                row += 1
-                qweight[row] |= (intweight[i] >> 1) & 0x3
-                i += 1
-                for j in range(i, i + 10):
-                    qweight[row] |= intweight[j] << (3 * (j - i) + 2)
-                i += 10
-                row += 1
             else:
-                raise NotImplementedError("Only 2,3,4,8 bits are supported.")
+                raise NotImplementedError("Only 2,4,8 bits are supported.")
                 
         qweight = qweight.astype(np.int32)
         self.qweight = torch.from_numpy(qweight) 
@@ -349,27 +306,8 @@ class QuantLinear(nn.Module):
                     qzeros[:, col] |= zeros[:, j] << (self.bits * (j - i))
                 i += 32//self.bits
                 col += 1
-            elif self.bits == 3:
-                for j in range(i, i + 10):
-                    qzeros[:, col] |= zeros[:, j] << (3 * (j - i))
-                i += 10
-                qzeros[:, col] |= zeros[:, i] << 30
-                col += 1
-                qzeros[:, col] |= (zeros[:, i] >> 2) & 1
-                i += 1
-                for j in range(i, i + 10):
-                    qzeros[:, col] |= zeros[:, j] << (3 * (j - i) + 1)
-                i += 10
-                qzeros[:, col] |= zeros[:, i] << 31
-                col += 1
-                qzeros[:, col] |= (zeros[:, i] >> 1) & 0x3
-                i += 1
-                for j in range(i, i + 10):
-                    qzeros[:, col] |= zeros[:, j] << (3 * (j - i) + 2)
-                i += 10
-                col += 1
             else:
-                raise NotImplementedError("Only 2,3,4,8 bits are supported.")
+                raise NotImplementedError("Only 2,4,8 bits are supported.")
                 
         qzeros = qzeros.astype(np.int32)
         self.qzeros = torch.from_numpy(qzeros) 
@@ -377,89 +315,15 @@ class QuantLinear(nn.Module):
     def forward(self, x):
         out_shape = x.shape[:-1] + (self.outfeatures, )
         x = x.reshape(-1,x.shape[-1])     
-        if self.is_trioton:
-            if self.bits in [2,4,8]:
-                out = torch.empty((x.shape[0], self.outfeatures), device='cuda', dtype=torch.float16)
-                grid = lambda META: (triton.cdiv(x.shape[0], META['BLOCK_SIZE_M']) * triton.cdiv(self.outfeatures, META['BLOCK_SIZE_N']),)
-                matmul_248_kernel[grid](x, self.qweight, out,
-                                        self.scales, self.qzeros, self.g_idx,
-                                        x.shape[0], self.outfeatures, self.infeatures, self.bits, self.maxq,
-                                        x.stride(0), x.stride(1),
-                                        self.qweight.stride(0), self.qweight.stride(1),
-                                        out.stride(0), out.stride(1),
-                                        self.scales.stride(0), self.qzeros.stride(0)) 
-            elif self.bits == 3:
-                if self.is_cuda is True and (self.kernel_switch_threshold is False or x.shape[0] < self.kernel_switch_threshold):
-                    out = torch.zeros((x.shape[0], self.outfeatures), device='cuda', dtype=torch.float32)
-                    quant_cuda.vecquant3matmul(x.float(), self.qweight, out, self.scales.float(), self.qzeros, self.g_idx)
-                    out = out.half()
-                else:
-                    zeros = self.qzeros.reshape(self.qzeros.shape[0], self.qzeros.shape[1]//3, 3, 1).expand(-1, -1, -1, 12)
-                    zeros = (zeros >> self.wf.unsqueeze(0))
-                    zeros[:,:,0,10] = (zeros[:,:,0,10]&0x3) | ((zeros[:,:,1,0] << 2)&0x4)
-                    zeros[:,:,1,11] = (zeros[:,:,1,11]&0x1) | ((zeros[:,:,2,0] << 1)&0x6)
-                    zeros = zeros & 0x7
-                    zeros = torch.cat([zeros[:,:,0,:11], zeros[:,:,1,1:12], zeros[:,:,2,1:11]], dim=2)
-                
-                    zeros = zeros + 1
-                    zeros = zeros.reshape(self.scales.shape)
-                    
-                    weight = self.qweight.reshape(self.qweight.shape[0]//3, 3, 1, self.qweight.shape[1]).expand(-1, -1, 12, -1)
-                    weight = (weight >> self.wf.unsqueeze(-1))&0x7
-                    weight[:,0,10] = (weight[:,0,10]&0x3) | ((weight[:,1,0] << 2)&0x4)
-                    weight[:,1,11] = (weight[:,1,11]&0x1) | ((weight[:,2,0] << 1)&0x6)
-                    weight = weight & 0x7
-                    weight = torch.cat([weight[:,0,:11], weight[:,1,1:12], weight[:,2,1:11]], dim=1)
-                    
-                    weight = weight.reshape(weight.shape[0] * weight.shape[1], weight.shape[2])
-                    
-                    weights = (self.scales[self.g_idx] * (weight - zeros[self.g_idx]))
-                    out = torch.matmul(x.half(), weights)
-        else:
-            if  self.is_cuda is True and (self.kernel_switch_threshold is False or x.shape[0] < self.kernel_switch_threshold):
-                out = torch.zeros((x.shape[0], self.outfeatures), device='cuda', dtype=torch.float32)
-                if self.bits == 2:
-                    quant_cuda.vecquant2matmul(x.float(), self.qweight, out, self.scales.float(), self.qzeros, self.g_idx)
-                elif self.bits == 3:
-                    quant_cuda.vecquant3matmul(x.float(), self.qweight, out, self.scales.float(), self.qzeros, self.g_idx)
-                elif self.bits == 4:
-                    quant_cuda.vecquant4matmul(x.float(), self.qweight, out, self.scales.float(), self.qzeros, self.g_idx)
-                elif self.bits == 8:
-                    quant_cuda.vecquant8matmul(x.float(), self.qweight, out, self.scales.float(), self.qzeros, self.g_idx)
-                out = out.half()
-            else:
-                if self.bits in [2,4,8]:
-                    zeros = torch.bitwise_right_shift(torch.unsqueeze(self.qzeros, 2).expand(-1, -1, 32 // self.bits), self.wf.unsqueeze(0)).to(torch.int16 if self.bits == 8 else torch.int8)
-                    torch.bitwise_and(zeros, (2 ** self.bits) - 1, out=zeros)
-                    
-                    zeros = zeros + 1
-                    zeros = zeros.reshape(self.scales.shape)   
-                            
-                    weight = torch.bitwise_right_shift(torch.unsqueeze(self.qweight, 1).expand(-1, 32 // self.bits, -1), self.wf.unsqueeze(-1)).to(torch.int16 if self.bits == 8 else torch.int8)
-                    torch.bitwise_and(weight,(2 ** self.bits) - 1, out=weight)
-                elif self.bits == 3:
-                    zeros = self.qzeros.reshape(self.qzeros.shape[0], self.qzeros.shape[1]//3, 3, 1).expand(-1, -1, -1, 12)
-                    zeros = (zeros >> self.wf.unsqueeze(0))
-                    zeros[:,:,0,10] = (zeros[:,:,0,10]&0x3) | ((zeros[:,:,1,0] << 2)&0x4)
-                    zeros[:,:,1,11] = (zeros[:,:,1,11]&0x1) | ((zeros[:,:,2,0] << 1)&0x6)
-                    zeros = zeros & 0x7
-                    zeros = torch.cat([zeros[:,:,0,:11], zeros[:,:,1,1:12], zeros[:,:,2,1:11]], dim=2)
-                
-                    zeros = zeros + 1
-                    zeros = zeros.reshape(self.scales.shape)  
-                
-                    weight = self.qweight.reshape(self.qweight.shape[0]//3, 3, 1, self.qweight.shape[1]).expand(-1, -1, 12, -1)
-                    weight = (weight >> self.wf.unsqueeze(-1))&0x7
-                    weight[:,0,10] = (weight[:,0,10]&0x3) | ((weight[:,1,0] << 2)&0x4)
-                    weight[:,1,11] = (weight[:,1,11]&0x1) | ((weight[:,2,0] << 1)&0x6)
-                    weight = weight & 0x7
-                    weight = torch.cat([weight[:,0,:11], weight[:,1,1:12], weight[:,2,1:11]], dim=1)
-                    
-                weight = weight.reshape(weight.shape[0] * weight.shape[1], weight.shape[2])
-                    
-                weights = (self.scales[self.g_idx] * (weight - zeros[self.g_idx]))
-                out = torch.matmul(x.half(), weights)
-                
+        out = torch.empty((x.shape[0], self.outfeatures), device='cuda', dtype=torch.float16)
+        grid = lambda META: (triton.cdiv(x.shape[0], META['BLOCK_SIZE_M']) * triton.cdiv(self.outfeatures, META['BLOCK_SIZE_N']),)
+        matmul_248_kernel[grid](x, self.qweight, out,
+                                self.scales, self.qzeros, self.g_idx,
+                                x.shape[0], self.outfeatures, self.infeatures, self.bits, self.maxq,
+                                x.stride(0), x.stride(1),
+                                self.qweight.stride(0), self.qweight.stride(1),
+                                out.stride(0), out.stride(1),
+                                self.scales.stride(0), self.qzeros.stride(0))
         out = out.reshape(out_shape)
         out = out + self.bias if self.bias is not None else out
         return out
