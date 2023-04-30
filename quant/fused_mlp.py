3f8259b42a761a72df5dbd171ff10308d6141846
qwopqwop200 <qwopqwop200@gmail.com>
2023-04-30 12:11:10 +0900

    add fused config

--- quant/fused_mlp.py
+++ quant/fused_mlp.py
@@ -66,6 +66,12 @@ try:
                 'BLOCK_SIZE_K': 64,
                 'GROUP_SIZE_M': 8
             }, num_stages=4, num_warps=4),  # 3090
+            triton.Config({
+                'BLOCK_SIZE_M': 64,
+                'BLOCK_SIZE_N': 32,
+                'BLOCK_SIZE_K': 64,
+                'GROUP_SIZE_M': 8
+            }, num_stages=4, num_warps=4),  # 3090
         ],
         key=['M', 'N', 'K'],
         nearest_power_of_two=True,
