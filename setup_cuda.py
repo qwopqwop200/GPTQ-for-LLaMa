bfc5f316efef1da84fdbd21734ce6a26444c74b1
James Zhao <jyzhao.work@gmail.com>
2023-05-17 23:19:41 -0700

    Add -O3 flag to nvcc

--- setup_cuda.py
+++ setup_cuda.py
@@ -4,7 +4,8 @@ from torch.utils import cpp_extension
 setup(
     name='quant_cuda',
     ext_modules=[cpp_extension.CUDAExtension(
-        'quant_cuda', ['quant_cuda.cpp', 'quant_cuda_kernel.cu']
+        'quant_cuda', ['quant_cuda.cpp', 'quant_cuda_kernel.cu'],
+        extra_compile_args={'nvcc': ['-O3']}
     )],
     cmdclass={'build_ext': cpp_extension.BuildExtension}
 )
